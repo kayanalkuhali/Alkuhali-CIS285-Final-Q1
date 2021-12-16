70ad46f86174b8cedfd411a9255c771b63b2e013
kayanalkuhali <alkuhali@umich.edu>
2021-12-16 17:33:08 -0500

    changes

--- final_22/testSelectionSort.java
+++ final_22/testSelectionSort.java
@@ -29,72 +29,51 @@ public  testSelectionSort () {
 	    arr[2] = 7;
 	    arr[3] = 10;
 	    arr[4] = 2;
-	    
+
 	    int[] Sortedarr = new int[5];
 	    Sortedarr[0] = 2;
 	    Sortedarr[1] = 7;
 	    Sortedarr[2] = 8;
 	    Sortedarr[3] = 9;
 	    Sortedarr[4] = 10;
-	    
-	    
+
+
 	    SelectionSort test = new  SelectionSort();
 	    int [] ss1 =test.sort(arr);
 	    assertArrayEquals("Test positive fails ",ss1,Sortedarr);
 	}
-	
-	
-	
+
+
+
 	public void testNegative(){
-		
+
 		int[] arr = new int[5];
 	    arr[0] = -11;
 	    arr[1] = -9;
 	    arr[2] = -2;
 	    arr[3] = -10;
 	    arr[4] = -6;
-	    
+
 	    int[] Sortedarr = new int[5];
 	    Sortedarr[0] = -11;
 	    Sortedarr[1] = -10;
 	    Sortedarr[2] = -9;
 	    Sortedarr[3] = -6;
 	    Sortedarr[4] = -2;
-	    
+
 	    SelectionSort test2 = new  SelectionSort();
 	    int [] ss2 =test2.sort(arr);
 	    assertArrayEquals("Test negative fails",ss2,Sortedarr);
 	    System.out.println("negative sorted array =" + Arrays.toString(ss2));
 
-		
-		
-		
-	}
-	
-	
-	public void testMixed(){
-		
-		
-		int[] arr = new int[5];
-	    arr[0] = -8;
-	    arr[1] = 3;
-	    arr[2] = -2;
-	    arr[3] =  4;
-	    arr[4] =  -7;
-	    
-	    int[] Sortedarr = new int[5];
-	    Sortedarr[0] = -8;
-	    Sortedarr[1] = -7;
-	    Sortedarr[2] = -2;
-	    Sortedarr[3] =  3;
-	    Sortedarr[4] =  4;
-	    
-	    SelectionSort test3 = new  SelectionSort();
-	    int [] ss3 =test3.sort(arr);
-	    assertArrayEquals("Test duplicates fails",ss3,Sortedarr);
-	    System.out.println("Duplicates sorted array =" + Arrays.toString(ss3));
 
-	    
+
+
+
+
+
+
+
 	    /** Test data contains with both positive, negative and zeros **/
 	}
 
