2a431dd54939ac4fafb5b2bde51d0d1c2185c7ee
kayanalkuhali <alkuhali@umich.edu>
2021-12-16 17:38:18 -0500

    new

--- final_22/testSelectionSort.java
+++ final_22/testSelectionSort.java
@@ -60,7 +60,7 @@ public  testSelectionSort () {
 	    Sortedarr[2] = -9;
 	    Sortedarr[3] = -6;
 	    Sortedarr[4] = -2;
-			//l
+			//ln
 
 	    SelectionSort test2 = new  SelectionSort();
 	    int [] ss2 =test2.sort(arr);
